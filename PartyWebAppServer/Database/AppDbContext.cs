f340fc61479e744b9cdeea227a0ec2f13ad7beea
Várnai Dávid <77694441+varnaidvid@users.noreply.github.com>
2024-05-22 13:20:36 +0200

    wallet model and its corresponding modelbuilder DONE

--- PartyWebAppServer/Database/AppDbContext.cs
+++ PartyWebAppServer/Database/AppDbContext.cs
@@ -10,16 +10,19 @@ public class AppDbContext : DbContext
     public AppDbContext()
     {
     }
-    
+
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-    
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<User>().HasKey(u => u.Username);
         modelBuilder.UseSerialColumns();
+
+        modelBuilder.Entity<Wallet>().HasKey(w => new { w.Currency, w.UserID });
+        modelBuilder.Entity<Wallet>().HasOne(w => w.Owner).WithMany(u => u.Wallets).HasForeignKey(w => w.UserID);
     }
 }
