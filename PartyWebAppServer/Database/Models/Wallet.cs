84513f816311e384dfa2fc95545be39a795ce983
vebesz <103445555+vebesz@users.noreply.github.com>
2024-05-22 13:42:07 +0200

    fixed wallet model

--- PartyWebAppServer/Database/Models/Wallet.cs
+++ PartyWebAppServer/Database/Models/Wallet.cs
@@ -39,43 +39,4 @@ public class Wallet
     {
         return $"Wallet: {Currency} {Amount}";
     }
-}using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-public class Wallet
-{
-    public enum CurrencyType
-    {
-        HUF,
-        EUR,
-        USD,
-        CREDIT
-    }
-
-    [Key]
-    public CurrencyType Currency { get; set; }
-
-    [Key]
-    [ForeignKey("User")]
-    public int UserID { get; set; }
-    public User Owner { get; set; }
-
-    public decimal Amount { get; set; }
-
-
-    public Wallet(CurrencyType currency, decimal amount, User owner)
-    {
-        Currency = currency;
-        Amount = amount;
-        Owner = owner;
-    }
-
-    public Wallet()
-    {
-    }
-
-    public override string ToString()
-    {
-        return $"Wallet: {Currency} {Amount}";
-    }
 }
