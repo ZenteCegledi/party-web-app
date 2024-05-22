c38098ef94817ca6f061d573e8d87be6c5b0191c
mbalint121 <madai.balint@gmail.com>
2024-05-22 13:29:55 +0200

    Created "Locations" table
    Making the "Locations" database table.

---
+++ PartyWebAppServer/Database/Models/Locations.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyWebAppServer.Database.Models;
+
+public class Locations
+{
+    public enum LocationType
+    {
+        club,
+        pub,
+        atm,
+        theater,
+        museum
+    }
+    
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public LocationType Type { get; set; }
+}
