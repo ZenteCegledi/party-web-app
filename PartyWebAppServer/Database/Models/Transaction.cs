657583ce3dd81d40eb869474fb6871ee9e0b8bdc
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-22 14:01:31 +0200

    Transaction Table
    Created Transaction table in database

---
+++ PartyWebAppServer/Database/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyWebAppServer.Database.Models;
+
+public class Transaction
+{
+    [Key] public int Id { get; set; }
+    public User User { get; set; }
+    public Wallet Wallet { get; set; }
+    public int SpentCurrency { get; set; }
+    public int Count { get; set; }
+    public Location? Location { get; set; }
+    public Event? Event { get; set; }
+    public TransactionTypes TransactionType { get; set; }
+    public DateTime Date { get; set; }
+    
+    public enum TransactionTypes
+    {
+        Food,
+        Ticket,
+        Deposit,
+        Credit
+    }
+}
