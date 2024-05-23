73210bf6169b25a3866f78b94caf81b16a365049
vebesz <103445555+vebesz@users.noreply.github.com>
2024-05-23 13:33:15 +0200

    login endpoint fixed

--- PartyWebAppServer/Services/AuthService.cs
+++ PartyWebAppServer/Services/AuthService.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PartyWebAppServer.Database;
+using PartyWebAppServer.Database.Models;
 
 namespace PartyWebAppServer.Controllers;
 
@@ -13,14 +14,14 @@ namespace PartyWebAppServer.Controllers;
 [Route("api/auth")]
 public class AuthController
 {
-    
+
     public AuthController(AppDbContext dbContext, IHttpContextAccessor httpContextAccessor)
     {
         DbContext = dbContext;
         HttpContextAccessor = httpContextAccessor;
-        
+
     }
-    
+
     private AppDbContext DbContext { get; set; }
     private IHttpContextAccessor HttpContextAccessor { get; set; }
     [HttpGet("me")]
@@ -31,39 +32,42 @@ public class AuthController
             var username = HttpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Name)?.Value;
             var role = HttpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Role)?.Value;
             var user = DbContext.Users.Where(u => u.Username == username).FirstOrDefault();
-            
+
             return new JsonResult(new { user.Username, user.Name, user.BirthDate, user.Email, user.Phone, role });
         }
-        
-        return new JsonResult(new { user = "not authenticated"});
-    }
-    
-    [HttpPost("login")]
-    public async Task<ClaimsIdentity> Login(SignInData data)
-    {
-            if (await DbContext.Users.AnyAsync(u => u.Username == data.Username && u.Password == data.Password))
-            {
-                var user = await DbContext.Users.Where(u => u.Username == data.Username).FirstOrDefaultAsync();
-                var role = await DbContext.Roles.Where(r => r.Id == user.RoleId).Select(r => r.Name).FirstOrDefaultAsync();
-                var cookieAndAuthTokenExpiration = DateTimeOffset.UtcNow.AddMinutes(5);
-                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
-                identity.AddClaim(new Claim(ClaimTypes.Name, data.Username));
-                identity.AddClaim(new Claim(ClaimTypes.Role, role.ToString()));
-                identity.AddClaim(new Claim(ClaimTypes.Expiration, cookieAndAuthTokenExpiration.ToString()));
-                
-                // im not sure this is needed here on the server
-                await HttpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity), new AuthenticationProperties {
-                        // ExpiresUtc = cookieAndAuthTokenExpiration,
-                         IsPersistent = true,
-                    });
-                
-                
-                return identity;
-            }
 
-            return new ClaimsIdentity();
+        return new JsonResult(new { user = "not authenticated" });
     }
-    
+
+    [HttpPost("login")]
+    public async Task<User?> Login(SignInData data)
+    {
+        if (await DbContext.Users.AnyAsync(u => u.Username == data.Username && u.Password == data.Password))
+        {
+            var user = await DbContext.Users.Where(u => u.Username == data.Username).FirstOrDefaultAsync();
+            var role = await DbContext.Roles.Where(r => r.Id == user.RoleId).Select(r => r.Name).FirstOrDefaultAsync();
+            var cookieAndAuthTokenExpiration = DateTimeOffset.UtcNow.AddMinutes(5);
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, data.Username));
+            identity.AddClaim(new Claim(ClaimTypes.Role, role.ToString()));
+            identity.AddClaim(new Claim(ClaimTypes.Expiration, cookieAndAuthTokenExpiration.ToString()));
+
+            // im not sure this is needed here on the server
+            await HttpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity), new AuthenticationProperties
+            {
+                // ExpiresUtc = cookieAndAuthTokenExpiration,
+                IsPersistent = true,
+            });
+
+
+            return user;
+        }
+
+        return null;
+
+
+    }
+
     [HttpPost("logout")]
     public async Task<IActionResult> Logout()
     {
