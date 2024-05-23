dfb4bfa09610b89f93eefa29ed44986f78823db7
Virágh Balázs <viraghbalazs2005@gmail.com>
2024-05-23 14:31:56 +0200

    Created requests, made changes to endpoints, fixed migration

--- PartyWebAppCommon/Requests/EditEventRequest.cs
+++ PartyWebAppCommon/Requests/EditEventRequest.cs
@@ -1,11 +1,11 @@
-﻿using PartyWebAppCommon.enums;
+using PartyWebAppCommon.enums;
 
 namespace PartyWebAppCommon.Requests;
 
 public class EditEventRequest
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; }
     public EventType Type {get ; set;}
     public int LocationId { get; set; }
     public int Price { get; set; }
