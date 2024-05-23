5b1587f426b26248f52648eccecd2b08d8047ffa
Virágh Balázs <viraghbalazs2005@gmail.com>
2024-05-23 10:38:43 +0200

    "GetEventById" and "CreateEvent" created

---
+++ PartyWebAppServer/Controllers/EventService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyWebAppCommon.enums;
+using PartyWebAppServer.Database;
+using PartyWebAppServer.Database.Models;
+
+namespace PartyWebAppServer.Controllers;
+
+[ApiController]
+[Route("api/events")]
+public class EventService
+{
+    private AppDbContext DbContext { get; set; }
+    
+    public EventService(AppDbContext dbContext)
+    {
+        DbContext = dbContext;
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<Event> GetEventById(int id)
+    {
+        Event events = DbContext.Events.ToList().Where(e => e.Id == id).ToList().First();
+        return events;
+    }
+    
+    // GetEventsByLocation
+
+    [HttpPost()]
+    public async Task<Event> CreateEvent(Event CreateEventRequest)
+    {
+        Event newEvent = CreateEventRequest;
+        return newEvent;
+    }
+    
+    // DeleteEvent
+    
+    //EditEvent
+    
+}
