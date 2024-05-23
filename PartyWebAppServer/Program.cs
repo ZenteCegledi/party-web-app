13c361453ae91620e65c6cc73d1724507aab0ce8
Várnai Dávid <77694441+varnaidvid@users.noreply.github.com>
2024-05-23 13:22:00 +0200

    dynamic header

--- PartyWebAppServer/Program.cs
+++ PartyWebAppServer/Program.cs
@@ -1,6 +1,3 @@
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using PartyWebAppServer.Database;
 using BitzArt.Blazor.Auth;
