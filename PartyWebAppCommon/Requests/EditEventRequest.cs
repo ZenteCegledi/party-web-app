bc4b2891722f04244c6cdb7785ded20d5ceabcab
Virágh Balázs <viraghbalazs2005@gmail.com>
2024-05-23 15:02:10 +0200

    Created IEventService & EventController
    WIP

--- PartyWebAppCommon/Requests/EditEventRequest.cs
+++ PartyWebAppCommon/Requests/EditEventRequest.cs
@@ -5,8 +5,8 @@ namespace PartyWebAppCommon.Requests;
 public class EditEventRequest
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public EventType Type {get ; set;}
+    public string? Name { get; set; }
+    public EventType? Type {get ; set;}
     public int LocationId { get; set; }
-    public int Price { get; set; }
+    public int? Price { get; set; }
 }
