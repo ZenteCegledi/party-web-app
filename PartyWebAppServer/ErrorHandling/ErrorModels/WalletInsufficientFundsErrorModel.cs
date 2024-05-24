cd2885718e37f2f9ce879d088ff88c18217d22d7
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-24 13:51:41 +0200

    Wallet Error Handling
    Exception when spent currency exceeds funds.
    
    Co-Authored-By: Gerencserihuba <119397701+Gerencserihuba@users.noreply.github.com>

---
+++ PartyWebAppServer/ErrorHandling/ErrorModels/WalletInsufficientFundsErrorModel.cs
@@ -0,0 +1,9 @@
+﻿using PartyWebAppCommon.enums;
+
+namespace PartyWebAppServer.ErrorHandling.ErrorModels;
+
+public class WalletInsufficientFundsErrorModel
+{
+    public int Amount { get; set; }
+    public CurrencyType Currency { get; set; }
+}
