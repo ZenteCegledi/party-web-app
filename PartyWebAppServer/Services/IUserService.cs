63cf9d26bcf602c694edf4a33e5186e88cefa683
Zente Hollo Cegledi <czh@czh.hu>
2024-05-24 13:55:48 +0200

    Update

--- PartyWebAppServer/Services/IUserService.cs
+++ PartyWebAppServer/Services/IUserService.cs
@@ -1,26 +1,10 @@
-<<<<<<< Updated upstream
-using Microsoft.AspNetCore.Mvc;
-=======
 using PartyWebAppCommon.Requests;
->>>>>>> Stashed changes
 using PartyWebAppServer.Database.Models;
 
 namespace PartyWebAppServer.Services
 {
     public interface IUserService
     {
-<<<<<<< Updated upstream
-        Task<ActionResult<List<User>>> GetAllUsers();
-
-        Task<ActionResult<User>> CreateUserRequest(string username, string name, DateTime birthDate, string email,
-            string phone, string password, List<Wallet> wallets);
-
-        Task<ActionResult<User>> GetUser(string username);
-        Task<ActionResult<User>> DeleteUser(string username);
-
-        Task<ActionResult<User>> EditUser(string username, string name, DateTime birthDate, string email,
-            string phone, string password, List<Wallet> wallets);
-=======
         Task<List<User?>> GetAllUsers();
 
         Task<User?> CreateUser(CreateUserRequest user);
@@ -29,6 +13,5 @@ namespace PartyWebAppServer.Services
         Task<User?> DeleteUser(string username);
 
         Task<User?> EditUser(string username, string? name, DateTime? birthDate, string? email, string? phone, string? password);
->>>>>>> Stashed changes
     }
 }
