00a03911712c58a1e7a1159800a7ea242511a66f
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-24 13:51:41 +0200

    Wallet Error Handling
    Exception when spent currency exceeds funds.
    
    Co-Authored-By: Gerencserihuba <119397701+Gerencserihuba@users.noreply.github.com>

---
+++ PartyWebAppServer/ErrorHandling/Exceptions/WalletInsufficientFundsAppException.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using PartyWebAppCommon.enums;
+using PartyWebAppServer.ErrorHandling.ErrorModels;
+
+namespace PartyWebAppServer.ErrorHandling.Exceptions;
+
+public class WalletInsufficientFundsAppException : AppException
+{
+    public WalletInsufficientFundsAppException(int amount, CurrencyType currency)
+    {
+        Message = $"{amount} {currency} exceeds wallet funds.";
+        ErrorObject = new WalletInsufficientFundsErrorModel{Amount = amount, Currency = currency};
+        HttpStatusCode = HttpStatusCode.BadRequest;
+    }
+    public override string Message { get; }
+    public override HttpStatusCode HttpStatusCode { get; }
+}
