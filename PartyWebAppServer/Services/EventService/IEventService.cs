ae9842dd567f42b26c123bd18fb2d506755b7cd2
Robby <schindler.robert_2020c@bolyai-szakkozep.hu>
2024-05-24 10:52:51 +0200

    Made "IEventService" & Changed EventService

--- PartyWebAppServer/Services/EventService/IEventService.cs
+++ PartyWebAppServer/Services/EventService/IEventService.cs
@@ -1,6 +1,12 @@
-﻿namespace PartyWebAppServer.Services.EventService;
+﻿using PartyWebAppServer.Database.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
+namespace PartyWebAppServer.Services.EventService;
 public interface IEventService
 {
-    // Event Service Interfaceje ide kell, ez fogja megvalósítani az EventService-t
+    Task<IEnumerable<Event>> GetAllEventsAsync();
+    Task<Event> GetEventByIdAsync(string id);
+    Task<Event> CreateEventAsync(Event newEvent);
+    Task UpdateEventAsync(Event updatedEvent);
 }
