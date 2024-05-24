2c2a7f74756fba3fc78d558d6fb305c5edbbc3b4
Virágh Balázs <viraghbalazs2005@gmail.com>
2024-05-24 14:18:21 +0200

    Readded M.Folder
    Fixing WIP

---
+++ PartyWebAppServer/Migrations/20240522120726_Wallet model.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PartyWebAppServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Walletmodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Wallet",
+                columns: table => new
+                {
+                    Currency = table.Column<int>(type: "integer", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wallet", x => new { x.Currency, x.Username });
+                    table.ForeignKey(
+                        name: "FK_Wallet_Users_Username",
+                        column: x => x.Username,
+                        principalTable: "Users",
+                        principalColumn: "Username",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wallet_Username",
+                table: "Wallet",
+                column: "Username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Wallet");
+        }
+    }
+}
