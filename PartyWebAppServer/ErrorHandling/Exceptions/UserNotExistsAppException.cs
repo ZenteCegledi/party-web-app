edc5a0237545f7298e10b202a9f7430a4028d497
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-24 13:03:10 +0200

    User Error Handling
    Created exception when user does not exist.

---
+++ PartyWebAppServer/ErrorHandling/Exceptions/UserNotExistsAppException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using PartyWebAppServer.ErrorHandling.ErrorModels;
+
+namespace PartyWebAppServer.ErrorHandling.Exceptions;
+
+public class UserNotExistsAppException : AppException
+{
+    public UserNotExistsAppException(string username)
+    {
+        Message = $"User with '{username}' username does not exist.";
+        ErrorObject = new UserNotExistsErrorModel{ Username = username};
+        HttpStatusCode = HttpStatusCode.BadRequest;
+    }
+    public override string Message { get; }
+    public override HttpStatusCode HttpStatusCode { get; }
+}
