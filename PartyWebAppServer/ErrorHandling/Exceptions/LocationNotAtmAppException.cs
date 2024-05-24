221206631db1ea5f43f8ecb0a772d9ea9955c1f2
Gerencserihuba <119397701+Gerencserihuba@users.noreply.github.com>
2024-05-24 13:56:14 +0200

    new error handler and fixes
    fixes on error handlers, added error handler for Event not exists, changed arguments types in transaction controller
    
    Co-Authored-By: Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>

--- PartyWebAppServer/ErrorHandling/Exceptions/LocationNotAtmAppException.cs
+++ PartyWebAppServer/ErrorHandling/Exceptions/LocationNotAtmAppException.cs
@@ -9,7 +9,7 @@ public class LocationNotAtmAppException : AppException
     public LocationNotAtmAppException(LocationType type)
     {
         Message = $"Cannot Deposit from {type.ToString()}";
-        ErrorObject = new  LocationNotAtmErrorModel(){LocationType = type.ToString()};
+        ErrorObject = new  LocationNotAtmErrorModel{LocationType = type.ToString()};
         HttpStatusCode = HttpStatusCode.BadRequest;
     }
     public override string Message { get; }
