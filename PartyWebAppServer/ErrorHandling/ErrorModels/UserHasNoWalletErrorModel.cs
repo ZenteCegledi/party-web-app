b6a02e565a0e1092f6a4c541a019538934fb8e30
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-24 13:22:53 +0200

    Wallet Error Handler
    Created exception handling for Wallet when User has no wallet with said Currency

---
+++ PartyWebAppServer/ErrorHandling/ErrorModels/UserHasNoWalletErrorModel.cs
@@ -0,0 +1,9 @@
+﻿using PartyWebAppCommon.enums;
+
+namespace PartyWebAppServer.ErrorHandling.ErrorModels;
+
+public class UserHasNoWalletErrorModel
+{
+    public string Username { get; set; }
+    public CurrencyType Currency { get; set; }
+}
---
+++ PartyWebAppServer/ErrorHandling/Exceptions/UserHasNoWalletAppException.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using PartyWebAppCommon.enums;
+using PartyWebAppServer.ErrorHandling.ErrorModels;
+
+namespace PartyWebAppServer.ErrorHandling.Exceptions;
+
+public class UserHasNoWalletAppException : AppException
+{
+    public UserHasNoWalletAppException(string username, CurrencyType currencyType)
+    {
+        Message = $"User '{username}' has no wallet with currency type '{currencyType.ToString()}'";
+        ErrorObject = new UserHasNoWalletErrorModel{Username = username, Currency = currencyType};
+        HttpStatusCode = HttpStatusCode.BadRequest;
+    }
+    public override string Message { get; }
+    public override HttpStatusCode HttpStatusCode { get; }
+}
