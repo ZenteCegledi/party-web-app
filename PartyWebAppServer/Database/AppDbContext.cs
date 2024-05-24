c60cd93da10269e0fe8d14e12300e677f14fc832
vebesz <103445555+vebesz@users.noreply.github.com>
2024-05-24 10:06:12 +0200

    db seed in progress

--- PartyWebAppServer/Database/AppDbContext.cs
+++ PartyWebAppServer/Database/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PartyWebAppCommon.enums;
 using PartyWebAppServer.Database.Models;
 
 namespace PartyWebAppServer.Database;
@@ -8,11 +9,11 @@ public class AppDbContext : DbContext
     public DbSet<User> Users { get; set; }
 
     public DbSet<Locations> Locations { get; set; }
-    
+
     public DbSet<Event> Events { get; set; }
-    
+
     public DbSet<Role> Roles { get; set; }
-    
+
     public AppDbContext()
     {
     }
@@ -30,12 +31,21 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<User>().HasKey(u => u.Username);
         modelBuilder.Entity<Wallet>().HasKey(w => new { w.Currency, w.Username });
         modelBuilder.Entity<Wallet>().HasOne(w => w.Owner).WithMany(u => u.Wallets).HasForeignKey(w => w.Username);
-        
+
         modelBuilder.Entity<Locations>().HasKey(l => l.Id);
-        
+
         modelBuilder.Entity<Role>().HasKey(r => r.Id);
         modelBuilder.Entity<Role>().Property(r => r.Name).HasConversion<string>();
 
-        
+
+        // Seed data ========================================
+
+        // Roles
+        modelBuilder.Entity<Role>().HasData(new Role { Id = 1, Name = RoleType.Admin });
+        modelBuilder.Entity<Role>().HasData(new Role { Id = 2, Name = RoleType.User });
+
+        // Users
+        // modelBuilder.Entity<User>().HasData(new User { Username = "admin", Password = "admin", RoleId = 1, Email = "admin@admin.com"});
+
     }
 }
