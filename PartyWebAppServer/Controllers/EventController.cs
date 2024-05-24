41f56b390949a52aed8ced43c911041390237f71
Virágh Balázs <viraghbalazs2005@gmail.com>
2024-05-24 14:43:59 +0200

    EventController & IEventService fixes

--- PartyWebAppServer/Controllers/EventController.cs
+++ PartyWebAppServer/Controllers/EventController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PartyWebAppCommon.Requests;
 using PartyWebAppServer.Database.Models;
 using PartyWebAppServer.Services.EventService;
 
@@ -15,15 +16,39 @@ public class EventController
         EventService = eventService;
     }
     
-    /*
-    [HttpGet("findall")]
-    public async Task<List<Event>> FindAllEvents()
+    [HttpGet()]
+    public async Task<List<Event>> GetAllEvents()
     {
-        List<Event> events = EventService.GetAllEvents();
-        return events;
-    }*/
+        return await EventService.GetAllEvents();
+    }
     
+    [HttpGet("{id}")]
+    public async Task<Event> GetEventById(int id)
+    {
+        return await EventService.GetEventById(id);
+    }
     
+    [HttpGet("findbylocationids")]
+    public async Task<List<Event>> GetEventByLocationIds(EventsByLocationRequest request)
+    {
+        return await EventService.GetEventByLocationIds(request);
+    }
     
-   
+    [HttpPost()]
+    public async Task<Event> CreateEvent(CreateEventRequest request)
+    {
+        return await EventService.CreateEvent(request);
+    }
+    
+    [HttpPut()]
+    public async Task<Event> EditEvent(EditEventRequest request)
+    {
+        return await EventService.EditEvent(request);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<Event> DeleteEvent(int id)
+    {
+        return await EventService.DeleteEvent(id);
+    }
 }
