1a9faadb1509dcbae70d7397ea9d0f1eae30c8cb
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-27 10:16:34 +0200

    Seperated to TransactionService
    Created TransactionService and placed TrancsactionController's functions into it.

--- PartyWebAppServer/Controllers/TransactionController.cs
+++ PartyWebAppServer/Controllers/TransactionController.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PartyWebAppServer.Database;
 using PartyWebAppServer.Database.Models;
 using PartyWebAppCommon.enums;
 using PartyWebAppServer.ErrorHandling.Exceptions;
+using PartyWebAppServer.Services.TransactionService;
 
 namespace PartyWebAppServer.Controllers;
 
@@ -11,69 +13,31 @@ namespace PartyWebAppServer.Controllers;
 public class TransactionController
 {
     private AppDbContext DbContext { get; set; }
+    private TransactionService TransactionService { get; set; }
 
     public TransactionController(AppDbContext dbContext)
     {
         DbContext = dbContext;
+        TransactionService = new TransactionService { DbContext = dbContext };
     }
 
     [HttpGet()]
-    public async Task<List<Transaction>> GetTransactions() => DbContext.Transactions.ToList();
+    public async Task<List<Transaction>> GetTransactions() => TransactionService.GetTransactions();
 
     [HttpGet("{username}")]
-    public async Task<List<Transaction>> GetTransaction(string username) =>
-        DbContext.Transactions.Where(t => t.Wallet.Owner.Username == username).OrderBy(t => t.Date).ToList();
-    
+    public async Task<List<Transaction>> GetUserTransactions(string username) =>
+        TransactionService.GetUserTransactions(username);
+
     [HttpGet("{transactionType}")]
-    public async Task<List<Transaction>> GetTransaction(TransactionType transactionType) =>
-        DbContext.Transactions.Where(t => t.TransactionType == transactionType).OrderBy(t => t.Date).ToList();
+    public async Task<List<Transaction>> GetTransactionsByType(TransactionType transactionType) =>
+        TransactionService.GetTransactionsByType(transactionType);
 
     [HttpPost("{id},{username},{spentCurrency},{count},{locationId},{eventId},{transactionType},{date}")]
-    public async void NewTransactionRequest(int id, string username, int spentCurrency, CurrencyType currencyType, int count,
+    public async Task<Transaction> NewTransactionRequest(int id, string username, int spentCurrency, CurrencyType currencyType, int count,
         int locationId, int eventId, TransactionType transactionType, DateTime date)
     {
-        Location? location = DbContext.Locations.FirstOrDefault(l => l.Id == locationId);
-        Event? currentEvent = DbContext.Events.FirstOrDefault(e => e.Id == eventId);
-        Wallet? wallet = DbContext.Wallets.FirstOrDefault(w => w.Owner.Username == username && w.Currency == currencyType);
-        User? user = DbContext.Users.FirstOrDefault(u => u.Username == username);
-        
-        if (user == null) throw new UserNotExistsAppException(username);
-        
-        switch (transactionType)
-        {
-            case TransactionType.Food:
-                if (location == null) throw new LocationNotExistsAppException(locationId);
-                if (location.Type == LocationType.ATM) throw new LocationIsAtmAppException(transactionType);
-                if (wallet == null) throw new UserHasNoWalletAppException(username, currencyType);
-
-                if (wallet.Amount < spentCurrency) 
-                    throw new WalletInsufficientFundsAppException(spentCurrency, currencyType);
-                break;
-            case TransactionType.Ticket:
-                if (location == null) throw new LocationNotExistsAppException(locationId);
-                if (location.Type == LocationType.ATM) throw new LocationIsAtmAppException(transactionType);
-                if (currentEvent == null) throw new EventNotExistsAppException(eventId);
-                if (wallet == null) throw new UserHasNoWalletAppException(username, currencyType);
-                
-                if (wallet.Amount < spentCurrency)
-                    throw new WalletInsufficientFundsAppException(spentCurrency, currencyType);
-                break;
-            case TransactionType.Deposit:
-                if (location == null) throw new LocationNotExistsAppException(locationId);
-                if (location.Type != LocationType.ATM) throw new LocationNotAtmAppException(location.Type);
-
-                if (wallet == null)
-                    wallet = new Wallet { Currency = currencyType, Owner = user, Amount = 0};
-                break;
-            case TransactionType.Credit:
-                location = null;
-                currentEvent = null;
-                if (wallet == null) throw new UserHasNoWalletAppException(username, currencyType);
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(transactionType), transactionType, "Transaction type does not exist.");
-        }
-        
-        Transaction transaction = new Transaction{ Id = id, Wallet = wallet, SpentCurrency = spentCurrency, Count = count, Location = location, Event = currentEvent, TransactionType = transactionType, Date = date};
+        Transaction transaction = TransactionService.NewTransactionRequest(id, username, spentCurrency, currencyType, count, locationId, eventId, transactionType, date);
+        TransactionService.AddTransactionToDb(transaction);
+        return transaction;
     }
 }
