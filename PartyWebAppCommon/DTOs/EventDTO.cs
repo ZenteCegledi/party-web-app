5867bc569bc03763738fff22f7ae61dd2cbd5e7e
Virágh Balázs <viraghbalazs2005@gmail.com>
2024-05-27 11:10:24 +0200

    Updated EventService
    Added DTOs

---
+++ PartyWebAppCommon/DTOs/EventDTO.cs
@@ -0,0 +1,12 @@
+﻿using PartyWebAppCommon.enums;
+
+namespace PartyWebAppCommon.DTOs;
+
+public class EventDTO
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public EventType Type { get; set; }
+    public int LocationId { get; set; }
+    public int Price { get; set; }
+}
--- PartyWebAppServer/Services/EventService/IEventService.cs
+++ PartyWebAppServer/Services/EventService/IEventService.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PartyWebAppCommon.DTOs;
 using PartyWebAppCommon.Requests;
 
 namespace PartyWebAppServer.Services.EventService;
 public interface IEventService
 {
-    public Task<List<Event>> GetAllEvents();
-    public Task<Event> GetEventById(int id);
-    public Task<List<Event>> GetEventByLocationIds([FromQuery]EventsByLocationRequest request);
-    public Task<Event> CreateEvent(CreateEventRequest request);
-    public Task<Event> EditEvent(EditEventRequest request);
-    public Task<Event> DeleteEvent(int id);
+    public Task<List<EventDTO>> GetAllEvents();
+    public Task<EventDTO> GetEventById(int id);
+    public Task<List<EventDTO>> GetEventByLocationIds([FromQuery]EventsByLocationRequest request);
+    public Task<EventDTO> CreateEvent(CreateEventRequest request);
+    public Task<EventDTO> EditEvent(EditEventRequest request);
+    public Task<EventDTO> DeleteEvent(int id);
 }
 
