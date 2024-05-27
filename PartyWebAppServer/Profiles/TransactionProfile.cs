8985bdba754694e0d278d66ef2bbd4410021c488
Gerencserihuba <119397701+Gerencserihuba@users.noreply.github.com>
2024-05-27 13:38:59 +0200

    TransactionDto Created
    Created TransactionProfile and TransactionDto files

---
+++ PartyWebAppServer/Profiles/TransactionProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using PartyWebAppCommon.DTOs;
+
+namespace PartyWebAppServer.Profiles;
+
+public class TransactionProfile : Profile
+{
+    public TransactionProfile()
+    {
+        CreateMap<Database.Models.Transaction, TransactionDto>();
+    }
+}
