4f5fd2f1f4b3a25854fdeb70cf538bee372ebe5a
Várnai Dávid <77694441+varnaidvid@users.noreply.github.com>
2024-05-28 10:34:01 +0200

    sync

--- PartyWebAppServer/Services/ServerWalletService.cs
+++ PartyWebAppServer/Services/ServerWalletService.cs
@@ -3,6 +3,8 @@ using PartyWebAppCommon.DTOs;
 using PartyWebAppCommon.Enums;
 using PartyWebAppServer.Database;
 using PartyWebAppServer.ErrorHandling.Exceptions;
+using PartyWebAppServer.Database.Models;
+using System.ComponentModel;
 
 namespace PartyWebAppServer.Services;
 
@@ -44,6 +46,110 @@ public class ServerWalletService(AppDbContext context)
             Username = wallet.Username,
             Amount = wallet.Amount,
             IsPrimary = wallet.IsPrimary
+        };
+    }
+
+    public WalletDto CreateWallet(WalletDto wallet)
+    {
+        var user = context.Users.FirstOrDefault(u => u.Username == wallet.Username);
+        if (user == null) throw new Exception("No user found with that username: " + wallet.Username);
+
+        var newWallet = new Wallet
+        {
+            Currency = wallet.Currency,
+            Username = wallet.Username,
+            Amount = wallet.Amount,
+            IsPrimary = wallet.IsPrimary
+        };
+
+        context.Wallets.Add(newWallet);
+        context.SaveChanges();
+
+        return wallet;
+    }
+
+    public WalletDto DeleteWallet(string username, CurrencyType currency)
+    {
+        var user = context.Users.FirstOrDefault(u => u.Username == username);
+        if (user == null) throw new Exception("No user found with that username: " + username);
+
+        var wallet = context.Wallets.FirstOrDefault(w => w.Username == username && w.Currency == currency);
+        if (wallet == null) throw new WalletNotExistsAppException();
+
+        context.Wallets.Remove(wallet);
+        context.SaveChanges();
+
+        return new WalletDto
+        {
+            Currency = wallet.Currency,
+            Username = wallet.Username,
+            Amount = wallet.Amount,
+            IsPrimary = wallet.IsPrimary
+        };
+    }
+
+    public WalletDto DepositToWallet(WalletDto wallet, decimal amount)
+    {
+        var user = context.Users.FirstOrDefault(u => u.Username == wallet.Username);
+        if (user == null) throw new Exception("No user found with that username: " + wallet.Username);
+
+        var walletEntity = context.Wallets.FirstOrDefault(w => w.Username == wallet.Username && w.Currency == wallet.Currency);
+        if (walletEntity == null) throw new WalletNotExistsAppException();
+
+        walletEntity.Amount += amount;
+        context.SaveChanges();
+
+        return new WalletDto
+        {
+            Currency = walletEntity.Currency,
+            Username = walletEntity.Username,
+            Amount = walletEntity.Amount,
+            IsPrimary = walletEntity.IsPrimary
+        };
+    }
+
+    public WalletDto WithdrawFromWallet(WalletDto wallet, decimal amount)
+    {
+        var user = context.Users.FirstOrDefault(u => u.Username == wallet.Username);
+        if (user == null) throw new Exception("No user found with that username: " + wallet.Username);
+
+        var walletEntity = context.Wallets.FirstOrDefault(w => w.Username == wallet.Username && w.Currency == wallet.Currency);
+        if (walletEntity == null) throw new WalletNotExistsAppException();
+
+        if (walletEntity.Amount < amount) throw new Exception("Not enough money in the wallet");
+
+        walletEntity.Amount -= amount;
+        context.SaveChanges();
+
+        return new WalletDto
+        {
+            Currency = walletEntity.Currency,
+            Username = walletEntity.Username,
+            Amount = walletEntity.Amount,
+            IsPrimary = walletEntity.IsPrimary
+        };
+    }
+
+    public WalletDto SetPrimaryWallet(WalletDto wallet)
+    {
+        var user = context.Users.FirstOrDefault(u => u.Username == wallet.Username);
+        if (user == null) throw new Exception("No user found with that username: " + wallet.Username);
+
+        var walletEntity = context.Wallets.FirstOrDefault(w => w.Username == wallet.Username && w.Currency == wallet.Currency);
+        if (walletEntity == null) throw new WalletNotExistsAppException();
+
+        var primaryWallet = context.Wallets.FirstOrDefault(w => w.Username == wallet.Username && w.IsPrimary);
+        if (primaryWallet != null) primaryWallet.IsPrimary = false;
+
+        walletEntity.IsPrimary = true;
+        context.SaveChanges();
+
+        return new WalletDto
+        {
+            Currency = walletEntity.Currency,
+            Username = walletEntity.Username,
+            Amount = walletEntity.Amount,
+            IsPrimary = walletEntity.IsPrimary
         };
     }
 }
