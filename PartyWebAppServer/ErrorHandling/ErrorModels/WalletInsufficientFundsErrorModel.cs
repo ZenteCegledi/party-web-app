eb93cb4fbc6fedebb516bbd25c3868db757a47db
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-28 12:50:14 +0200

    Wallet Exception fixed

--- PartyWebAppServer/ErrorHandling/ErrorModels/WalletInsufficientFundsErrorModel.cs
+++ PartyWebAppServer/ErrorHandling/ErrorModels/WalletInsufficientFundsErrorModel.cs
@@ -4,6 +4,7 @@ namespace PartyWebAppServer.ErrorHandling.ErrorModels;
 
 public class WalletInsufficientFundsErrorModel
 {
+    public string Username { get; set; }
     public int Amount { get; set; }
     public CurrencyType Currency { get; set; }
 }
