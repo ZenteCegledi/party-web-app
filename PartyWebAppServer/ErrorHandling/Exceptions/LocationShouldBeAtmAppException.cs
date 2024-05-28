ada3337fb585cf38780ddc637eaa87fb97b75d92
Gerencserihuba <119397701+Gerencserihuba@users.noreply.github.com>
2024-05-28 12:06:53 +0200

    Fixes on error handlers

--- PartyWebAppServer/ErrorHandling/Exceptions/LocationShouldBeAtmAppException.cs
+++ PartyWebAppServer/ErrorHandling/Exceptions/LocationShouldBeAtmAppException.cs
@@ -9,7 +9,7 @@ public class LocationShouldBeAtmAppException : AppException
     public LocationShouldBeAtmAppException(LocationDTO location)
     {
         Message = $"Cannot Deposit from {location.Type}";
-        ErrorObject = new  LocationShouldBeAtmErrorModel{Location = location};
+        ErrorObject = new  LocationShouldBeAtmErrorModel{Name = location.Name, Address = location.Address};
         HttpStatusCode = HttpStatusCode.BadRequest;
     }
     public override string Message { get; }
