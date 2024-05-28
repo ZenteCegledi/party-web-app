531ee825265cf5c7af695bc22761f11e64eada91
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-28 11:29:55 +0200

    Implementing requests
    Started implementation of NewTransactionRequest

---
+++ PartyWebAppCommon/Requests/NewTransactionRequest.cs
@@ -0,0 +1,19 @@
+﻿using PartyWebAppCommon.DTOs;
+using PartyWebAppCommon.enums;
+
+namespace PartyWebAppCommon.Requests;
+
+public class NewTransactionRequest
+{
+    public UserDto User { get; set; }
+    public WalletDto Wallet { get; set; }
+    public int SpentCurrency { get; set; }
+    public int Count { get; set; }
+    
+    public LocationDTO? Location { get; set; }
+    public EventDto? Event { get; set; }
+    
+    public TransactionType TransactionType { get; set; }
+    
+    public DateTime Date { get; set; }
+}
