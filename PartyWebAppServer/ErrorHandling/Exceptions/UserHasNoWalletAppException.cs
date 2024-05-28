191cda8738f802a92f59a68d0187d52bd53fc8e1
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-28 15:03:24 +0200

    Applied class naming change

--- PartyWebAppServer/ErrorHandling/Exceptions/UserHasNoWalletAppException.cs
+++ PartyWebAppServer/ErrorHandling/Exceptions/UserHasNoWalletAppException.cs
@@ -1,13 +1,12 @@
 ﻿using System.Net;
 using PartyWebAppCommon.DTOs;
-using PartyWebAppCommon.enums;
 using PartyWebAppServer.ErrorHandling.ErrorModels;
 
 namespace PartyWebAppServer.ErrorHandling.Exceptions;
 
 public class UserHasNoWalletAppException : AppException
 {
-    public UserHasNoWalletAppException(UserDto user, WalletDto wallet)
+    public UserHasNoWalletAppException(UserDTO user, WalletDto wallet)
     {
         Message = $"User '{user.Username}' has no wallet with currency type '{wallet.Currency.ToString()}'";
         ErrorObject = new UserHasNoWalletErrorModel{Username = user.Username, Currency = wallet.Currency};
