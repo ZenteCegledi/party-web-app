146a49ecb7001d42315a98f75d38c7a0262cd317
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-28 11:29:12 +0200

    Changed error

--- PartyWebAppServer/ErrorHandling/Exceptions/LocationNotExistsAppException.cs
+++ PartyWebAppServer/ErrorHandling/Exceptions/LocationNotExistsAppException.cs
@@ -1,15 +1,16 @@
 ﻿using System.Net;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PartyWebAppCommon.DTOs;
 using PartyWebAppServer.ErrorHandling.ErrorModels;
 
 namespace PartyWebAppServer.ErrorHandling.Exceptions;
 
 public class LocationNotExistsAppException : AppException
 {
-    public LocationNotExistsAppException(int locationId)
+    public LocationNotExistsAppException(LocationDTO location)
     {
-        Message = $"Location with id: '{locationId}' does not exist.";
-        ErrorObject = new LocationNotExistsErrorModel{LocationId = locationId};
+        Message = $"Location with name:'{location.Name}' and address:'{location.Address}' does not exist in DB.";
+        ErrorObject = new LocationNotExistsErrorModel{Name = location.Name, Address = location.Address};
         HttpStatusCode = HttpStatusCode.BadRequest;
     }
     public override string Message { get; }
