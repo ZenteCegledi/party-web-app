ae1908362f606089a3de3295c8f460f38b621349
Várnai Dávid <77694441+varnaidvid@users.noreply.github.com>
2024-05-29 14:01:17 +0200

    .

--- PartyWebAppServer/Database/Models/Transaction.cs
+++ PartyWebAppServer/Database/Models/Transaction.cs
@@ -7,20 +7,19 @@ namespace PartyWebAppServer.Database.Models;
 public class Transaction
 {
     [Key] public int Id { get; set; }
-    
-    [ForeignKey("Wallet")] public CurrencyType WalletCurrency { get; set; }
-    [ForeignKey("Wallet")] public string WalletUsername { get; set; }
+
+    [ForeignKey("Username, Currency")]
     public Wallet Wallet { get; set; }
-    
+
     [ForeignKey("Location")] public int LocationId { get; set; }
     public Location? Location { get; set; }
-    
+
     [ForeignKey("Event")] public int EventId { get; set; }
     public Event? Event { get; set; }
-    
+
     public int SpentCurrency { get; set; }
     public int Count { get; set; }
-    
+
     public TransactionType TransactionType { get; set; }
     public DateTime Date { get; set; }
 }
