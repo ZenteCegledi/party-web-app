a23a39b18c9e984331e51adc1da9f784a7c1b2cc
Lábodi Attila <134223035+eLArcangeles@users.noreply.github.com>
2024-05-29 14:19:01 +0200

    Added Wallet Id

--- PartyWebAppServer/Database/Models/Wallet.cs
+++ PartyWebAppServer/Database/Models/Wallet.cs
@@ -6,15 +6,14 @@ namespace PartyWebAppServer.Database.Models;
 
 public class Wallet
 {
-    [Key]
-    public CurrencyType Currency { get; set; }
-
-    [Key]
-    [ForeignKey("User")]
-    public string Username { get; set; }
-
+    public int Id { get; set; }
+    
+    [ForeignKey("User")] public string Username { get; set; }
     public User Owner { get; set; }
+    
     public decimal Amount { get; set; }
+    public CurrencyType Currency { get; set; }
+    
     public bool IsPrimary { get; set; } = false;
 
     public List<Transaction> Transactions { get; set; }
