ed1641a7a1df6565f2622d3bd96a356d7d763d8f
Várnai Dávid <77694441+varnaidvid@users.noreply.github.com>
2024-05-30 14:46:26 +0200

    deleted temp transactions

--- PartyWebAppClient/Components/Dashboard/Transactions.razor.cs
+++ PartyWebAppClient/Components/Dashboard/Transactions.razor.cs
@@ -8,8 +8,6 @@ namespace PartyWebAppClient.Components.Dashboard;
 
 public partial class Transactions : ComponentBase
 {
-    private List<TransactionDto> tempTransactions = new List<TransactionDto>();
-    
     [Inject]
     private ITransactionService transactionService { get; set; }
 
@@ -25,19 +23,6 @@ public partial class Transactions : ComponentBase
     
     protected override async Task OnInitializedAsync()
     {
-        for (int i = 0; i < 5; i++)
-        {
-            tempTransactions.Add(new TransactionDto
-            {
-                SpentCurrency = Random.Next(100, 1000),
-                WalletId = 1,
-                Date = DateTime.Now.ToUniversalTime(),
-                Id = i,
-                TransactionType = (TransactionType)(i),
-                Count = 1,
-            });
-        }
-        
         var (transactions, error) = await transactionService.GetWalletTransactions(chosenWallet.Id);
         if (error is not null)
         {
