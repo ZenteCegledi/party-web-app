1b12fa792f7e0f9e7edebb63cfcd363b31c5ee37
Gerencserihuba <119397701+Gerencserihuba@users.noreply.github.com>
2024-05-30 13:54:17 +0200

    Transaction Dto duplicte removed added correct file

---
+++ PartyWebAppCommon/DTOs/TransactionDto.cs
@@ -0,0 +1,11 @@
+﻿using PartyWebAppCommon.Enums;
+
+namespace PartyWebAppCommon.DTOs;
+
+public class TransactionDto
+{
+    public int SpentCurrency { get; set; }
+    public int Count { get; set; }
+    public TransactionType TransactionType { get; set; }
+    public DateTime Date { get; set; }
+}
