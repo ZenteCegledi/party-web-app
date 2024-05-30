a990521f212d1c557c1049dee76e238e63459fd9
Robby <schindler.robert_2020c@bolyai-szakkozep.hu>
2024-05-30 14:42:04 +0200

    fixed commit

--- PartyWebAppClient/Services/UserService/IUserService.cs
+++ PartyWebAppClient/Services/UserService/IUserService.cs
@@ -1,6 +1,15 @@
-﻿namespace PartyWebAppClient.Services.UserService;
+﻿using PartyWebAppClient.Models;
+using PartyWebAppCommon.DTOs;
+using PartyWebAppCommon.Enums;
+using PartyWebAppCommon.Requests;
 
-public class IUserService
+namespace PartyWebAppClient.Services.UserService;
+
+public interface IUserService
 {
+    Task<(List<UserDto>, AppErrorModel)> GetAllUsers(int Id);
+    Task<(UserDto, AppErrorModel)> CreateUser(CreateUserRequest _req);
+    Task<(UserDto, AppErrorModel)> EditUser(EditUserRequest _req);
+    Task<(UserDto, AppErrorModel)> DeleteUser(int Id);
     
 }
--- PartyWebAppClient/Services/UserService/UserService.cs
+++ PartyWebAppClient/Services/UserService/UserService.cs
@@ -1,6 +1,22 @@
-﻿namespace PartyWebAppClient.Services.UserService;
+﻿using PartyWebAppClient.Models;
+using PartyWebAppCommon.DTOs;
+using PartyWebAppCommon.Enums;
+using PartyWebAppCommon.Requests;
 
-public class UserService
+namespace PartyWebAppClient.Services.UserService;
+
+public class UserService(IAppHttpClient http) : IUserService
 {
+    private IUserService _userServiceImplementation;
+
+    public async Task<(List<UserDto>, AppErrorModel)> GetAllUsers(int Id) =>
+        await http.GetAsync<List<UserDto>>($"http://localhost:5259/api/user/{Id}");
     
+    public async Task<(UserDto, AppErrorModel)> CreateUser(CreateUserRequest _req) =>
+        await http.PostAsync<UserDto>($"http://localhost:5259/api/newuser", _req);
+
+    public async Task<(UserDto, AppErrorModel)> EditUser(EditUserRequest _req) =>
+        await http.PutAsync<UserDto>($"http://localhost:5259/api/user", _req);
+    public async Task<(UserDto, AppErrorModel)> DeleteUser(int Id) =>
+        await http.DeleteAsync<UserDto>($"http://localhost:5259/api/user/{Id}");
 }
